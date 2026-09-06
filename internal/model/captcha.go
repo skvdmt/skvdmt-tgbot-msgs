1e6c7871a7eaf0a394163df8324057b981cad3fd
skvdmt <skvdmt@yandex.ru>
2026-09-06 06:27:35 +0300

    fixed

--- internal/model/captcha.go
+++ internal/model/captcha.go
@@ -2,18 +2,14 @@ package model
 
 import (
 	"github.com/skvdmt/captcha"
-	"github.com/skvdmt/captcha/config"
 )
 
 // Captcha Создать каптчу и получить ее изображение и значение.
 func Captcha() (image []byte, value string, err error) {
-	capt, err := captcha.New(&captcha.Config{
-		FontFiles: Config.Fonts,
-		FontSizes: &config.FontSizes{
-			Min: 80,
-			Max: 120,
-		},
-	})
+	capt, err := captcha.New(
+		captcha.WithFontFiles(Config.Fonts),
+		captcha.WithFontSizes(80, 120),
+	)
 	if err != nil {
 		return nil, "", err
 	}
